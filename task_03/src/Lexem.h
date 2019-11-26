a550d52e4cb62cfb934e91d4f5ce9ee55d981350
Ilya Pogodin <54841469+June102@users.noreply.github.com>
2019-11-26 12:03:11 +0300

    Update Lexem.h

--- task_03/src/Lexem.h
+++ task_03/src/Lexem.h
@@ -9,43 +9,79 @@
 
 
 enum tokens {
-    unknown_tk = -1,    // we get unknown token
-    program_tk = 0,     // 'program'
-    var_tk,             // 'var'
-    begin_tk,           // 'begin'
-    end_tk,             // 'end'
-    type_tk,            // 'type'
-    id_tk       = 8,    // any [aA-zZ][0-9]
-    constant_tk = 9,    // 0-9
-    dot_tk,             // '.'
-    comma_tk,           // ','
-    ddt_tk,             // ':'
-    semi_tk,            // ';'
-    eqv_tk,             // '='
-    ass_tk,             // ':='
-    add_tk,             // '+'
-    sub_tk,             // '-'
-    mul_tk,             // '*'
-    div_tk,             // '/'
-    opb_tk,             // '('
-    cpb_tk,             // ')'
+	unknown_tk = -1,    // we get unknown token
+	program_tk = 0,     // 'program'
+	var_tk,             // 'var'
+	begin_tk,           // 'begin'
+	end_tk,             // 'end'
+	type_tk,            // 'type'
+	id_tk = 8,    // any [aA-zZ][0-9]
+	constant_tk = 9,    // 0-9
+	if_tk,				// 'if'
+	then_tk,			// 'then'
+	else_tk,			// 'else'
+	for_tk,				// 'for'
+	while_tk,			// 'while'
+	do_tk,				// 'do'
+	or_tk,				// 'or'
+	and_tk,				// 'and'
+	xor_tk,				// 'xor'
+	nand_tk,			// 'nand'
+	of_tk,				// 'of'
+	to_tk,				// 'to'
+	downto_tk,			// 'downto'
+	array_tk,			// 'array'
+	dot_tk,             // '.'
+	comma_tk,           // ','
+	ddt_tk,             // ':'
+	semi_tk,            // ';'
+	eqv_tk,             // '='
+	ass_tk,             // ':='
+	add_tk,             // '+'
+	sub_tk,             // '-'
+	mul_tk,             // '*'
+	div_tk,             // '/'
+	opb_tk,             // '('
+	cpb_tk,             // ')'
+	osb_tk,				// '['
+	csb_tk,				// ']'
+	eq_tk,				// '=='
+	neq_tk,				// '<>'
+	ge_tk,				// '>'
+	lw_tk,				// '<'
+	geq_tk,				// '>='
+	leq_tk,				// '<='
 
-    eof_tk              // end of file
+	eof_tk              // end of file
 };
 
 
 enum errors {
-    UNKNOWN_LEXEM,   // Unknown lexeme
-    EOF_ERR,         // End of file error
-    MUST_BE_ID,      // Error: Must be identifier
-    MUST_BE_SEMI,    // Error: Must be ';'
-    MUST_BE_PROG,    // Error: Must be 'program'
-    MUST_BE_COMMA,   // Error: Must be ','
-    MUST_BE_TYPE,    // Error: Must be type of identifier
-    MUST_BE_DOT,     // Error: Must be '.'
-    MUST_BE_ASS,     // Error: Must be ':='
-    DUPL_ID_ERR,     // Error: Duplicate declaration on identifier
-    UNKNOWN_ID,      // Error: Undefined identifier
+	UNKNOWN_LEXEM,   // Unknown lexeme
+	EOF_ERR,         // End of file error
+	MUST_BE_ID,      // Error: Must be identifier
+	MUST_BE_SEMI,    // Error: Must be ';'
+	MUST_BE_PROG,    // Error: Must be 'program'
+	MUST_BE_COMMA,   // Error: Must be ','
+	MUST_BE_TYPE,    // Error: Must be type of identifier
+	MUST_BE_DOT,     // Error: Must be '.'
+	MUST_BE_ASS,     // Error: Must be ':='
+	DUPL_ID_ERR,     // Error: Duplicate declaration on identifier
+	UNKNOWN_ID,      // Error: Undefined identifier
+
+	MUST_BE_OSB,	 // Error: Must be '['
+	MUST_BE_CONST,	 // Error: Must be constant
+	MUST_BE_CSB,	 // Error: Must be ']'
+	MUST_BE_OF,		 // Error: Must be 'of'
+	MUST_BE_TYPE_OR_ARRAY, // Error: Must be type or 'array'
+	VAR_IS_NOT_ARRAY,// Error: Variable must be array
+	UNACC_EXP,		 // Error: Unacceptable expression
+	MUST_BE_ID_OR_CONST,   // Error: Must be identifier or constant
+	MUST_BE_STATE,	 // Error: Must be some statement
+	MUST_BE_DO,		 // Error: Must be 'do'
+	MUST_BE_TO,		 // Error: Must be 'to' or 'downto'
+	UNACC_RANGE,	 // Error: Unacceptable range for cycle
+	MUST_BE_CPB,	 // Error: Must be ')'
 };
 
 
