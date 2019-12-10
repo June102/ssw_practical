b4be41b6b3cb6c77b431c0adcee2de2b612e0f18
Ilya Pogodin <54841469+June102@users.noreply.github.com>
2019-12-10 16:27:18 +0300

    Update Lexem.h

--- task_03/src/Lexem.h
+++ task_03/src/Lexem.h
@@ -23,6 +23,8 @@ enum tokens {
 	for_tk,				// 'for'
 	while_tk,			// 'while'
 	do_tk,				// 'do'
+	repeat_tk,			// 'repeat'
+	until_tk,			// 'until'
 	or_tk,				// 'or'
 	and_tk,				// 'and'
 	xor_tk,				// 'xor'
@@ -45,7 +47,6 @@ enum tokens {
 	cpb_tk,             // ')'
 	osb_tk,				// '['
 	csb_tk,				// ']'
-	eq_tk,				// '=='
 	neq_tk,				// '<>'
 	ge_tk,				// '>'
 	lw_tk,				// '<'
@@ -74,14 +75,16 @@ enum errors {
 	MUST_BE_CSB,	 // Error: Must be ']'
 	MUST_BE_OF,		 // Error: Must be 'of'
 	MUST_BE_TYPE_OR_ARRAY, // Error: Must be type or 'array'
-	VAR_IS_NOT_ARRAY,// Error: Variable must be array
+	NO_INDEX_OF_ARRAY,// Error: Variable must be array
 	UNACC_EXP,		 // Error: Unacceptable expression
 	MUST_BE_ID_OR_CONST,   // Error: Must be identifier or constant
 	MUST_BE_STATE,	 // Error: Must be some statement
 	MUST_BE_DO,		 // Error: Must be 'do'
 	MUST_BE_TO,		 // Error: Must be 'to' or 'downto'
-	UNACC_RANGE,	 // Error: Unacceptable range for cycle
 	MUST_BE_CPB,	 // Error: Must be ')'
+	MUST_BE_UNTIL,	 // Error: Must be 'until'
+	UNACC_TYPE,		 // Error: Unacceptble type of identifier
+	MUST_BE_THEN,	 // Error: Must be 'then'
 };
 
 
